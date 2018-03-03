74be88f3d65afdb910f12f98acf2516a9d1b624f
myapos <myapos@yahoo.com>
2018-03-03 17:29:07 +0200

    dlooping an array with draw results in display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,42 +14,57 @@ import { Data } from './models';
 
 import { Effects } from './effects';
 
+//https://angular.io/guide/displaying-data
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
   <h2>Hello world from {{title}}</h2>
-  Chunks: <br>
-  
-    <div *ngIf="apiData$ | async as d">
-      test
-      <h2> {{ d.login}} </h2>
-      <h2> {{ d.id}} </h2>
-      <h2> {{ d.avatar_url}} </h2>
-    </div>
+  Display Draws: <br>
+
+  <div *ngIf="apiData$ | async as dd">
+      <div *ngFor="let d of dd">
+            <h2>Draw</h2>
+            <h3> Draw Number: {{ d.drawNo}} </h3>
+            <h3> Time: {{ d.drawTime}} </h3>
+            <h3> Results: {{ d.results}} </h3>
+      </div>
+      <div>Results {{dd[0].drawNo}}</div>
+  </div>
   `,
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent implements OnInit{
-  title = 'Redux-Angular-App';
+  title = 'Forecasting Kino Results';
   // loadDataSuccess$ : Observable<any>;
   // apiData$: Observable<Data>;
   apiData$: Observable<any>;
 
-    // private store: Store<AppState>,
+  // private store: Store<AppState>,
   constructor(
     private store: Store<any>,
     private initializeActions: initializeActions,
     private Effects : Effects
    ) {
-    // debugger;
+
     this.apiData$ = this.store.select(state => {
       // debugger;
-      return state.apiData.data;
+      // return state.apiData.data.draws.draw ?  state.apiData.data.draws.draw[0] : '';
+      if(state.apiData.data.draws.draw) {
+        return state.apiData.data.draws.draw;
+      } else {
+        return [{
+              drawNo: 'Loading History Results',
+              drawTime: 'Loading History Results',
+              results: 'Loading History Results',
+            }
+
+        ];
+      }
+
 
     });
-    // debugger;
-    // console.log('data:', this.apiData$);
+
    
   }
 
